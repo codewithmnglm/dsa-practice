8769c3687e7db0efa99286b883151898ff2b63e3
Kumar Mangalam <kmrmnglm@gmail.com>
2025-10-23 18:59:55 +0530

    LC 75 - Sort Colors

--- src/test/java/com/practice/dsa/ArrayProblems.java
+++ src/test/java/com/practice/dsa/ArrayProblems.java
@@ -124,6 +124,49 @@ public class ArrayProblems {
         return count > 0 ? count : -1;
 
     }
+    public void sortColors(int[] nums) {
+
+        int zeroCount=0;
+        int oneCount=0;
+        int twoCount=0;
+
+        for (int i=0;i<nums.length;i++){
+
+            if(nums[i]==0) zeroCount++;
+
+            if(nums[i]==1) oneCount++;
+
+            if(nums[i]==2) twoCount++;
+
+
+        }
+
+        for(int i=0;i<nums.length;i++){
+
+            if(zeroCount>0){
+
+                nums[i]=0;
+                zeroCount--;
+
+            }
+            else if(oneCount>0){
+
+                nums[i]=1;
+                oneCount--;
+
+            }
+            else if(twoCount>0){
+
+                nums[i]=2;
+                twoCount--;
+
+            }
+
+
+
+        }
+
+    }
 
 
     public static void main(String[] args) {
