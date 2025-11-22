0c0b00a9f02f7164a0567fd8502ae9c49d291a84
Kumar Mangalam <kmrmnglm@gmail.com>
2025-11-23 01:36:54 +0530

    LC 930 - Binary Sum

--- src/test/java/com/practice/dsa/PrefixSum.java
+++ src/test/java/com/practice/dsa/PrefixSum.java
@@ -286,6 +286,31 @@ public class PrefixSum {
 
         return res;
     }
+    public static int numSubarraysWithSumLC930(int[] nums, int k) {
+
+
+        HashMap<Integer, Integer> hm = new HashMap<>();
+        int count = 0;
+
+        hm.put(0, 1);
+        int sum = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+
+            sum = sum + nums[i];
+
+
+            if (hm.containsKey(sum - k)) {
+
+                count = count + hm.get(sum - k);
+            }
+            hm.put(sum, hm.getOrDefault(sum, 0) + 1);
+        }
+
+
+        return count;
+    }
+
 
     public static void main(String[] args) {
 
