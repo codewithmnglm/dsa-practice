8630bb8f2edad8e8c404292bcc3e82795dfcf7ee
Kumar Mangalam <kmrmnglm@gmail.com>
2025-12-02 23:22:30 +0530

    LC20  Valid Parentheses

--- src/test/java/com/practice/dsa/Stacks.java
+++ src/test/java/com/practice/dsa/Stacks.java
@@ -1,4 +1,38 @@
 package com.practice.dsa;
 
+import java.util.Stack;
+
 public class Stacks {
+
+
+    public static boolean isValid(String s) {
+
+        Stack<Character> st = new Stack<>();
+
+        for (char c : s.toCharArray()) {
+
+
+            if (c == '{' || c == '[' || c == '(') st.push(c);
+
+            else if (st.isEmpty()) {
+                if (c == '}' || c == ']' || c == ')') return false;
+            } else if (c == '}' && st.peek() == '{') st.pop();
+
+            else if (c == ']' && st.peek() == '[') st.pop();
+            else if (c == ')' && st.peek() == '(') st.pop();
+
+            else if (st.peek() == '{' && c == ']' || c == ')') return false;
+
+            else if (st.peek() == '[' && c == ']' || c == '}') return false;
+
+            else if (st.peek() == '(' && c == ']' || c == '}') return false;
+
+        }
+        return st.isEmpty();
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(isValid("([)]"));
+    }
 }
